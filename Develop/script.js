6c259c9841f757f37d2fc6d572b53231ba5095ed
Wes Young <wesley.young@du.edu>
2020-10-22 13:11:09 -0600

    Added comments to JS and cleaned file, changed text color for past, present, and future classes in css, and deleted trial code from index file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,62 +1,8 @@
-//Attaching current day to currentDay id
+//Get local storage data
 var previousData = JSON.parse(localStorage.getItem("dayPlan"));
-$("#currentDay").text(moment().format("MMMM Do YYYY"));
-var militaryValue = 12;
-hourInput = $("<div>");
-$(".container").append(hourInput);
-timeArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-for (var i = 0; i < 9; i++) {
-  var timeBlock = $("<section>").attr("class", "row time-block");
-  $(hourInput).append(timeBlock);
-  var timeSlot = $("<h3>").attr("class", "hour col-2");
-  if (timeArray[i] === 9 || timeArray[i] === 10 || timeArray[i] === 11) {
-    timeSlot.text(timeArray[i] + "am");
-    timeSlot.attr("value", timeArray[i]);
-  } else {
-    timeSlot.text(timeArray[i] + "pm");
-    timeSlot.attr("value", militaryValue);
-    militaryValue++;
-  }
-  var eventArea = $("<textarea>").attr("class", "description col-9");
-  var saveButton = $("<button>").attr({
-    id: "button" + [i],
-    class: "saveBtn col-1",
-  });
-  if (previousData !== null) {
-    hourSavedData = timeSlot.attr("value");
-    console.log(hourSavedData);
-    console.log(previousData[hourSavedData]);
-    eventArea.val(previousData[hourSavedData]);
-  }
-  if (
-    $(timeSlot).attr("value") <
-    Number(moment().format()[11] + moment().format()[12])
-  ) {
-    eventArea.attr("class", "col-9 past");
-  } else if (
-    $(timeSlot).attr("value") ==
-    Number(moment().format()[11] + moment().format()[12])
-  ) {
-    eventArea.attr("class", "present col-9");
-  } else {
-    eventArea.attr("class", "future col-9");
-  }
-  $(timeBlock).append(timeSlot);
-  $(timeBlock).append(eventArea);
-  $(timeBlock).append(saveButton);
-}
-
-$(".time-block").on("click", ".saveBtn", function () {
-  hourSaved = $(this).siblings(".hour").attr("value");
-  console.log(hourSaved);
-  console.log($(this).siblings("textarea").val());
-  dayLog[hourSaved] = $(this).siblings("textarea").val();
-  console.log(dayLog);
-  var storeData = localStorage.setItem("dayPlan", JSON.stringify(dayLog));
-});
-
+//If local storage is empty, set empty object
 if (previousData === null) {
-  var dayLog = {
+  var previousData = {
     9: "",
     10: "",
     11: "",
@@ -67,10 +13,60 @@ if (previousData === null) {
     16: "",
     17: "",
   };
-} else {
-  var dayLog = previousData;
 }
-
-console.log(dayLog);
-
-// console.log(Number(moment().format()[11] + moment().format()[12]));
+//Add current day
+$("#currentDay").text(moment().format("MMMM Do YYYY"));
+//Create global variable to set values to each row and check for current time
+var militaryTime = 9;
+//For loop to create all the elements
+for (var i = 0; i < 9; i++) {
+  //Variables for the different elements as well as attaching their respective classes
+  var timeBlock = $("<section>").attr("class", "row time-block description");
+  var eventArea = $("<textarea>").attr("class", "col-9");
+  var saveButton = $("<button>").attr({
+    id: "button" + [i],
+    class: "saveBtn col-1",
+    value: militaryTime,
+  });
+  var saveButtonIcon = $("<i>").attr("class", "fas fa-save");
+  var timeSlot = $("<h5>").attr("class", "hour col-2");
+  //If statements to add appropriate text to each time slot
+  if (militaryTime < 12) {
+    timeSlot.text(militaryTime + " am");
+  } else if (militaryTime === 12) {
+    timeSlot.text(militaryTime + " pm");
+  } else {
+    timeSlot.text(militaryTime - 12 + " pm");
+  }
+  //Adds local storage text back to specific time slot
+  eventArea.val(previousData[militaryTime]);
+  //If statement to check if time slot is past, present, or future and attaches correct class
+  if (
+    $(saveButton).attr("value") <
+    Number(moment().format()[11] + moment().format()[12])
+  ) {
+    eventArea.attr("class", "col-9 past");
+  } else if (
+    $(saveButton).attr("value") ==
+    Number(moment().format()[11] + moment().format()[12])
+  ) {
+    eventArea.attr("class", "present col-9");
+  } else {
+    eventArea.attr("class", "future col-9");
+  }
+  //Appends the element to proper location
+  $(".container").append(timeBlock);
+  $(timeBlock).append(timeSlot, eventArea, saveButton);
+  $(saveButton).append(saveButtonIcon);
+  //Increments the time value variable
+  militaryTime++;
+}
+//Click event for save buttons
+$(".time-block").on("click", ".saveBtn", function () {
+  //Saves value from click button
+  hourSaved = $(this).attr("value");
+  //Adds text from textarea to clicked location in object
+  previousData[hourSaved] = $(this).siblings("textarea").val();
+  //Adds object to local storage
+  localStorage.setItem("dayPlan", JSON.stringify(previousData));
+});
